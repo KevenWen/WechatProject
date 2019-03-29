666d840f22654048e08db5db3342f35614c5c09c
KevenWen <Keven.wen@quest.com>
2019-03-29 16:35:39 +0800

    Add files

---
+++ WeChatHelloWorld1/WeChatHelloWorld1/Controllers/WXController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Xml;
+
+namespace WeChatHelloWorld1.Controllers
+{
+    public class WXController : Controller
+    {
+        // GET: WX
+        public ActionResult Index(string echoStr, string signature, string timestamp, string nonce)
+        {
+            string WeiXinToken = "zhousheQWER^^&&*";//要和你微信公众平台设置的保持一致          
+
+            if (CheckSignature(WeiXinToken, signature, timestamp, nonce))
+            {
+                if (!string.IsNullOrEmpty(echoStr))
+                {
+                    return Content(echoStr);
+                }
+            }
+
+            return Content("");
+        }
+
+        /// <summary>
+        /// 验证微信签名
+        /// </summary>
+        public static bool CheckSignature(string token, string signature, string timestamp, string nonce)
+        {
+            string[] ArrTmp = { token, timestamp, nonce };
+
+            Array.Sort(ArrTmp);
+            string tmpStr = string.Join("", ArrTmp);
+
+            tmpStr = FormsAuthentication.HashPasswordForStoringInConfigFile(tmpStr, "SHA1");
+            //tmpStr = Membership.CreateUser(tmpStr, "SHA1");
+            tmpStr = tmpStr.ToLower();
+
+            if (tmpStr == signature)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            string postString = string.Empty;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Request.InputStream);
+            return Content(TextHandle(doc));
+        }
+
+        /// <summary>
+        /// 处理信息并应答
+        /// </summary>
+        public string TextHandle(XmlDocument xmldoc)
+        {
+            string responseContent = "";
+            XmlNode ToUserName = xmldoc.SelectSingleNode("/xml/ToUserName");
+            XmlNode FromUserName = xmldoc.SelectSingleNode("/xml/FromUserName");
+            XmlNode Content = xmldoc.SelectSingleNode("/xml/Content");
+            if (Content != null)
+            {
+                responseContent = string.Format(Message_Text,
+                    FromUserName.InnerText,
+                    ToUserName.InnerText,
+                    DateTime.Now.Ticks,
+                    "您输入的内容为：" + Content.InnerText);
+            }
+            return responseContent;
+        }
+        /// <summary>
+        /// 普通文本消息
+        /// </summary>
+        public static string Message_Text
+        {
+            get { return @"<xml>
+                            <ToUserName><![CDATA[{0}]]></ToUserName>
+                            <FromUserName><![CDATA[{1}]]></FromUserName>
+                            <CreateTime>{2}</CreateTime>
+                            <MsgType><![CDATA[text]]></MsgType>
+                            <Content><![CDATA[{3}]]></Content>
+                            </xml>"; }
+        }
+
+
+
+       
+    }
+}
