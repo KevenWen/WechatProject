6323aa8c3f6ca7da2baad3dedea736da2c268100
Ricky Zhou <Ricky.Zhou@quest.com>
2019-05-24 16:52:52 +0800

    增加注册功能

---
+++ WeChatOrderingSystem/Controllers/MenuController.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Mvc;
+using Senparc.Weixin.MP.Containers;
+using Senparc.Weixin.MP.Entities.Menu;
+using Senparc.NeuChar;
+using Senparc.Weixin.MP.CommonAPIs;
+using Senparc.Weixin;
+
+namespace WeChatHelloWorld1.Controllers
+{
+    public class MenuController : Controller
+    {
+
+        public static readonly string AppId = Config.SenparcWeixinSetting.WeixinAppId;//与微信公众账号后台的AppId设置保持一致，区分大小写。
+        public static readonly string AppSecret = Config.SenparcWeixinSetting.WeixinAppSecret;
+
+
+        public ActionResult CreateMenuForMerchant()
+        {
+            ButtonGroup bg = new ButtonGroup();
+            // 添加注册菜单
+            bg.button.Add(new SingleViewButton()
+            {
+                name = "注册",
+                url = string.Format("https://open.weixin.qq.com/connect/oauth2/authorize?appid={0}&redirect_uri={1}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect", AppId, Url.Encode("https://www.share-parking.com/Register/")),
+                type = MenuButtonType.view.ToString(), 
+            });
+            bg.button.Add(new SingleViewButton()
+            {
+                name = "菜单",
+                url = string.Format("https://open.weixin.qq.com/connect/oauth2/authorize?appid={0}&redirect_uri={1}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect", AppId, Url.Encode("https://www.share-parking.com/Menu/")),
+                type = MenuButtonType.view.ToString(),
+                
+            });
+            bg.button.Add(new SingleViewButton()
+            {
+                name = "我的订单",
+                url = string.Format("https://open.weixin.qq.com/connect/oauth2/authorize?appid={0}&redirect_uri={1}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect", AppId, Url.Encode("https://www.share-parking.com/Order/")),
+                type = MenuButtonType.view.ToString(),
+
+            });
+            var accessToken = AccessTokenContainer.TryGetAccessToken(AppId, AppSecret);
+            var result = CommonApi.CreateMenu(accessToken, bg);
+
+            return Json(new
+            {
+                Success = result.errmsg == "ok",
+                Message = "菜单更新成功。"
+            }, JsonRequestBehavior.AllowGet);
+
+
+        }
+    }
+}
---
+++ WeChatOrderingSystem/Models/WeChatUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeChatHelloWorld1.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("WeChatUser")]
+    public partial class WeChatUser
+    {
+        [Key]
+        [StringLength(28)]
+        public string OpenID { get; set; }
+
+        [StringLength(100)]
+        public string Nickname { get; set; }
+
+        public int? Sex { get; set; }
+
+        [StringLength(30)]
+        public string Province { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(50)]
+        public string Country { get; set; }
+
+        [StringLength(200)]
+        public string HeadImgUrl { get; set; }
+
+        public DateTime? SubscribeTime { get; set; }
+
+        [StringLength(20)]
+        public string Language { get; set; }
+
+        public short? UserType { get; set; }
+  
+    }
+}
