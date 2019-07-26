a6fca3e71e1284c90702f9fee1f784a0bc0cf307
Ricky Zhou <Ricky.Zhou@quest.com>
2019-07-26 17:09:31 +0800

    pretty adding product page

--- WeChatOrderingSystem/Controllers/ProductsController.cs
+++ WeChatOrderingSystem/Controllers/ProductsController.cs
@@ -45,15 +45,14 @@ namespace WeChatHelloWorld1.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ProductID,MerchantID,Name,Price,Status,ImagePath,Description,Created,LatestModify,Comment")] Product product)
         {
-            if (ModelState.IsValid)
-            {
+            //if (ModelState.IsValid)
+            //{
                 db.Products.Add(product);
                 db.SaveChanges();
                 return RedirectToAction("Index");
-            }
+            //}
 
             return View(product);
         }
